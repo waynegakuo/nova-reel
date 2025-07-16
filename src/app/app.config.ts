6e9532d164876f443b59ec1a4c57e19d6bb7871e
Wayne Gakuo <waynejotham@gmail.com>
2025-07-16 17:52:14 +0300

    feat(functions): Introduce Firebase Functions with TypeScript setup
    - Bootstrapped Firebase Functions project using TypeScript and ESLint.
    - Added environment configurations for production and development.
    - Integrated Firebase Admin and Firebase Functions SDKs.
    - Configured TypeScript compiler options and ESLint settings.
    - Provided initial Firebase Functions structure with placeholders.
    - Updated project to use Firebase services within the Angular app.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,12 +4,15 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 import {provideHttpClient, withFetch} from '@angular/common/http';
+import {initializeApp, provideFirebaseApp} from '@angular/fire/app';
+import {environment} from '../environments/environment.development';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideClientHydration(withEventReplay()),
-    provideHttpClient(withFetch())
+    provideHttpClient(withFetch()),
+    provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
   ]
 };
--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,1 +1,12 @@
-export const environment = {};
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyChGxzLHydbRQrDQvz7H1Ncnb_SiffHMXI",
+    authDomain: "nova-reels.firebaseapp.com",
+    projectId: "nova-reels",
+    storageBucket: "nova-reels.firebasestorage.app",
+    messagingSenderId: "489441645492",
+    appId: "1:489441645492:web:536ebe1a6a486c03ce18e1",
+    measurementId: "G-CK5BYNQFY4"
+  }
+};
